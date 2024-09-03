336b227fd857450ef2c8df5590a114e1e77fa801
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-09-03 21:21:22 +0400

    feat: create middleware

---
+++ api/github/webhooks/index.js
@@ -0,0 +1,7 @@
+const { createNodeMiddleware, createProbot } = require("probot");
+const app = require("../../../index");
+
+module.exports = createNodeMiddleware(app, {
+  probot: createProbot(),
+  webhooksPath: "/api/github/webhooks",
+});
