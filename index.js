825be28ee03f83892ffa91838a6711722db2f1b8
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-09-03 21:57:54 +0400

    wip

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// For more information on building apps:
-// https://probot.github.io/docs/
-
-// To get your app running against GitHub, see:
-// https://probot.github.io/docs/development/
-
 const util = {
   production: true,
   tags: [
