a6cb77b7d40aace623a12a99898bda18cbaa9076
Kosta <68782786+KostaD02@users.noreply.github.com>
2024-09-03 21:47:54 +0400

    fix: import

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,6 +1,6 @@
 import { createNodeMiddleware, createProbot } from "probot";
 
-import app from "../../../app.js";
+import app from "../../../index.js";
 
 export default createNodeMiddleware(app, {
   probot: createProbot(),
